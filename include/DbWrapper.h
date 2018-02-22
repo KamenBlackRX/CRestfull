33a245fc6c49ff662735468aa0792bfe374c945a
Jefferson Puchalski <puchiruzuki@gmail.com>
2018-02-22 16:18:07 -0300

    Added libpq support, need fix in Makefile

--- include/DbWrapper.h
+++ include/DbWrapper.h
@@ -1,23 +1,34 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include "/usr/include/postgresql/libpq-fe.h"
 
 #pragma warning "This wrapper need implement in C code.\nNeed generate obj for linkage"
 
 extern "C"
 {
+
+    /**
+     * Exit and close connection in free thread way.
+     */
+    static void clean_exit(PGconn *conn);
+
     /**
      * Create connection and store his status response.
      * @param char** ip  Address for connection
      * @param char** FLAGS Default flags for connection
      */
-    int connection(char**, char**);
+    int connection(char*, char**);
 
     /**
      * Execute a non paginate query
      * @param char** type_sql  Path or query for exectuion
      * @return char** return the response of query.
      */
-    char** executeNonPaginateQuery(char**);
+    char* executeNonPaginateQuery(char*);
 
+    /* Private Variables */
+    PGconn      *conn;      // Connection Pointer
+    PGresult    *result;    //Result point for sql query.
+    const char  * info;     // flags info
 
 }
--- src/DbWrapper.cpp
+++ src/DbWrapper.cpp
@@ -1,15 +1,22 @@
 #include "../include/DbWrapper.h"
-#include <stdlib.h>
+
 extern "C"
 {
+
+    /* Exit connection with a clean exit */
+    static void clean_exit(PGconn *conn)
+    {
+        PQfinish(conn);
+    }
+
     /**
      * Create connection and store his status response.
      * @param char** ip  Address for connection
      * @param char** FLAGS Default flags for connection
      */
-    int connection(char** ip, char** FLAGS)
+    int connection(char* ip, char** FLAGS)
     {
-        return 0;
+
     }
 
     /**
@@ -17,9 +24,9 @@ extern "C"
      * @param char** type_sql  Path or query for exectuion
      * @return char** return the response of query.
      */
-    char** executeNonPaginateQuery(char** type_sql)
+    char* executeNonPaginateQuery(char* type_sql)
     {
-        char* response = (char*)calloc(256,sizeof(char));
-        return &response;
+        char* response = (char*)malloc(sizeof(char));
+        return response;
     }
 }
